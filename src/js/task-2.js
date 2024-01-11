bfd9a55e52df198313680e0ecae55cd6b21b478c
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-11 15:29:37 +0200

    Update task-2.js

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -1,5 +1,10 @@
 // task -2
 
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
+
 function makeNegative(num) {
   if (num > 0) {
     console.log(-(num));
@@ -11,5 +16,3 @@ function makeNegative(num) {
 
   return num;
 }
-
-console.log(makeNegative(1));
