1857dc4e43e83a6651cd02f2eb17172dae449004
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-11 15:26:28 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+// task-1
+
+/*
 function highAndLow(string) {
   const numbers = string.split(' ');
 
@@ -11,3 +14,20 @@ function highAndLow(string) {
 console.log(highAndLow('1 2 3 4 5')); // return '5 1'
 console.log(highAndLow('1 2 -3 4 5')); // return '5 -3'
 console.log(highAndLow('1 9 3 4 -5')); // return '9 -5'
+*/
+
+// task -2
+
+function makeNegative(num) {
+  if (num > 0) {
+    console.log(-(num));
+  } else if (num === 0) {
+    console.log(num);
+  } else {
+    console.log(num);
+  }
+
+  return num;
+}
+
+console.log(makeNegative(1));
