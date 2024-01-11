74daf95ea185fb1cfe860ffc1e12e367f879ce4e
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-11 15:26:54 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -15,19 +15,3 @@ console.log(highAndLow('1 2 3 4 5')); // return '5 1'
 console.log(highAndLow('1 2 -3 4 5')); // return '5 -3'
 console.log(highAndLow('1 9 3 4 -5')); // return '9 -5'
 */
-
-// task -2
-
-function makeNegative(num) {
-  if (num > 0) {
-    console.log(-(num));
-  } else if (num === 0) {
-    console.log(num);
-  } else {
-    console.log(num);
-  }
-
-  return num;
-}
-
-console.log(makeNegative(1));
