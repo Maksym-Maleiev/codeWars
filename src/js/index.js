9c3488f640cb89f08104481752cd78cdd26d6e6b
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-11 15:27:13 +0200

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 // task-1
 
-/*
 function highAndLow(string) {
   const numbers = string.split(' ');
 
@@ -14,4 +13,3 @@ function highAndLow(string) {
 console.log(highAndLow('1 2 3 4 5')); // return '5 1'
 console.log(highAndLow('1 2 -3 4 5')); // return '5 -3'
 console.log(highAndLow('1 9 3 4 -5')); // return '9 -5'
-*/
