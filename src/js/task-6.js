16322ccdc28d0fcd933947e31312a601670b531d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-13 17:08:09 +0200

    Update task-6.js

--- src/js/task-6.js
+++ src/js/task-6.js
@@ -1,5 +1,5 @@
-function getLargestNumber(...numbers) {
-  return Math.max(...numbers);
+function getLargestNumber([...numbers]) {
+  return Math.max([...numbers]);
 }
 
-console.log(getLargestNumber(1, 2, 4, 5, 86, 2, 6));
+console.log(getLargestNumber([1, 2, 4, 5, 86, 2, 6]));
